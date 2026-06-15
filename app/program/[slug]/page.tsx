4c0b096ef09dfa48bcb915a7fa3f4d58c77e1100
Aa Kasep <driher@gmail.com>
2026-06-15 20:20:02 +0700

    perbaikan detil agenda dan program

--- app/program/[slug]/page.tsx
+++ app/program/[slug]/page.tsx
@@ -113,23 +113,31 @@ export default async function ProgramDetailPage({
       {/* CONTENT */}
       <article
         className="
-          prose
-          prose-lg
-          max-w-none
+          prose prose-lg mt-10 max-w-none text-gray-700
+      prose-headings:text-gray-900
+      prose-headings:font-bold
+      prose-p:leading-8
+      prose-p:text-gray-700
+      prose-a:text-green-700
+      prose-img:rounded-2xl
+      prose-img:shadow-md
+      prose-strong:text-gray-900
+      prose-blockquote:border-green-600
+      prose-blockquote:text-gray-700
+      prose-li:marker:text-green-700
+      [&>*]:mb-4
+            [&>p]:mb-5
 
-          prose-headings:font-bold
-          prose-headings:text-slate-900
+            [&>h2]:mt-8 [&>h2]:mb-4 [&>h2]:text-2xl [&>h2]:font-bold
+            [&>h3]:mt-6 [&>h3]:mb-3 [&>h3]:text-xl [&>h3]:font-semibold
 
-          prose-p:text-slate-700
-          prose-p:leading-8
+            [&>ul]:mb-5 [&>ul]:pl-5 [&>ul]:list-disc
+            [&>ol]:mb-5 [&>ol]:pl-5 [&>ol]:list-decimal
 
-          prose-a:text-green-700
+            [&_img]:rounded-xl
+            [&_img]:w-full
 
-          prose-img:rounded-xl
-          prose-img:shadow-md
-
-          prose-blockquote:border-green-600
-          prose-blockquote:text-slate-600
+            [&>p>a]:text-orange-500 [&>p>a]:underline
         "
         dangerouslySetInnerHTML={{
           __html: program.content?.rendered || "",
