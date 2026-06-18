bd2087a2a83b34b4ff6f0f10e5a35079607a0e37
Aa Kasep <driher@gmail.com>
2026-06-18 23:32:15 +0700

    Perbaikan dan penyempurnaan

--- app/profil-mes/[slug]/page.tsx
+++ app/profil-mes/[slug]/page.tsx
@@ -92,11 +92,31 @@ export default async function ProfilMesPage({
 
         <article
           className="
-            prose
-            prose-lg
-            max-w-none
-            prose-img:rounded-2xl
-            prose-headings:font-bold
+            prose prose-lg mt-10 max-w-none text-gray-700
+      prose-headings:text-gray-900
+      prose-headings:font-bold
+      prose-p:leading-8
+      prose-p:text-gray-700
+      prose-a:text-green-700
+      prose-img:rounded-2xl
+      prose-img:shadow-md
+      prose-strong:text-gray-900
+      prose-blockquote:border-green-600
+      prose-blockquote:text-gray-700
+      prose-li:marker:text-green-700
+      [&>*]:mb-4
+            [&>p]:mb-5
+
+            [&>h2]:mt-8 [&>h2]:mb-4 [&>h2]:text-2xl [&>h2]:font-bold
+            [&>h3]:mt-6 [&>h3]:mb-3 [&>h3]:text-xl [&>h3]:font-semibold
+
+            [&>ul]:mb-5 [&>ul]:pl-5 [&>ul]:list-disc
+            [&>ol]:mb-5 [&>ol]:pl-5 [&>ol]:list-decimal
+
+            [&_img]:rounded-xl
+            [&_img]:w-full
+
+            [&>p>a]:text-orange-500 [&>p>a]:underline
           "
           dangerouslySetInnerHTML={{
             __html:
